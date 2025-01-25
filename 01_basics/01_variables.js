cfec7d3af17f8e8fc2a32be05227f0c523259bed
ranjana22bramhe <ranjana22bramhe@gmail.com>
2025-01-25 04:51:41 +0000

    detatyypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const  accountId=12345;
+let accountEmail="ran@gmail.com";
+var accountPassword="6789";
+accountCity="jaipur";
+console.log(accountId);
+// accountId=979789;
+accountEmail="ranbra@gmail.com";
+accountPassword="2222";
+accountCity="bangaluru"
+let accountState;
+console.log([accountId, accountEmail, accountPassword, accountCity]);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_dataTyypes.js
@@ -0,0 +1,22 @@
+"use strict" //treat all JS code as newer  verser
+//alert(3+3) // we are using nodejs, not browser
+console.log(3+3); 
+//code readability should be high
+console.log("Ranjana")
+let name="Ranjana"
+let age =18
+let isLoggedIn = false
+
+// number =>2 to power 53
+//bignint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefiend =>
+    //symbol
+
+//object
+
+console.log(typeof undefiend);//undefined
+console.log(typeof null) //object
+
