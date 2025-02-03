3772fe366cd630193cd7d94dfec905d7a2c657c3
ranjana22bramhe <ranjana22bramhe@gmail.com>
2025-02-03 05:18:38 +0000

    string method

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -12,3 +12,17 @@ const gameName =new String('ranjana')
 // console.log(gameName.toUpperCase());
 console.log(gameName.charAt(3));
 console.log(gameName.indexOf('j'));
+ const newString = gameName.substring(0,4)
+ console.log(newString);
+
+ const anotherString = gameName.slice(-8,4)
+ console.log(anotherString);
+ const newStringOne = "  ranjana "
+ console.log(newStringOne)
+ console.log(newStringOne.trim())
+
+
+ const url = "https://hitesh.com/hitesh%20choudhary"
+ console.log(url.replace('%20', "-"))
+ console.log(url.includes('sundar'))
+ console.log(gameName.split(" "));
